78358a7f3cc42f467d4bd1d72f0b686784d92fd3
Lance T <starrazor31@gmail.com>
2018-05-23 10:29:47 -0700

    Adding an easy editable config file for bot. This file should remain blank on github and should only be used as a template

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  options: {
+    debug: false
+  },
+  connection: {
+    cluster: 'aws',
+    reconnect: true
+  },
+  identity: {
+    username: '', // bot username
+    password: 'oauth:', // oauth token with the 'oauth:' prefix
+    oauth: '' // just the oauth token
+  },
+  // Array of channels for your bot to join
+  channels: [
+    ''
+  ]
+}
