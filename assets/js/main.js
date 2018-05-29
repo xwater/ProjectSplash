d62a8dd29db5e367edacf65357a6af1e3dd8bb4b
Lance T <starrazor31@gmail.com>
2018-05-29 13:17:49 -0700

    Adding Bootstrap to make the index page a little more nicer. Status messages for failing to connect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
 $(function () {
-  var connection = new WebSocket('ws://127.0.0.1:3000')
+  let start = $('#start')
+
+  const connection = new WebSocket('ws://127.0.0.1:3000')
   hidePlayers('')
   hidePlayers('sd')
-  $('#start').hide()
+  start.hide()
   connection.onopen = function () {
+    hideErrors()
     // connection is opened and ready to use
-    $('#counter').text('connection ready!')
+    showStatusMessage('connection ready!')
     connection.send('admin')
   }
 
   connection.onerror = function (error) {
     if (error) {
+      showErrors('Web socket server is not running. Please run npm run start from the root of the directory in your console.')
       throw (error)
     }
     // an error occurred when sending/receiving data
@@ -22,23 +26,27 @@ $(function () {
       const json = JSON.parse(message.data)
       console.log(json)
       if (json === 'ready') {
-        $('#start').show()
+        start.show()
         $('#generate').hide()
+      }
+      if (json === 'The animation is not connected') {
+        showErrors('The overlay window or character selection screen is not open. If they are open please refresh this page and the other two windows and try again.')
       }
       if (json === 'gamestart') {
         showPlayers('')
-        $('#start').hide()
+        start.hide()
       } else if (json === 'gamedone') {
         hidePlayers('')
         $('#generate').show()
-      }
-      // meant for sudden death scenario
-      else if (typeof json[1] !== 'undefined' && typeof json[2] !== 'undefined') {
+      } else if (typeof json[1] !== 'undefined' && typeof json[2] !== 'undefined') {
+        // meant for sudden death scenario
         if (json[2] !== 'SD') { return }
         console.log('doing the thing')
         hidePlayers('')
         $('#p' + (json[0].pos + 1)).show()
         $('#p' + (json[1].pos + 1)).show()
+      } else {
+        console.log(message)
       }
     } catch (e) {
       console.log('This doesn\'t look like a valid JSON: ', message.data)
@@ -71,10 +79,36 @@ $(function () {
   $('#p4').click(function () {
     connection.send('4')
   })
-  $('#start').click(function () {
+  start.click(function () {
     connection.send('start')
   })
   $('#generate').click(function () {
     connection.send('generate')
   })
 })
+
+function hideErrors () {
+  let errors = $('#error')
+  errors.hide()
+  errors.text('')
+}
+
+function showErrors (errorMsg) {
+  hideStatusMessage()
+  let errors = $('#error')
+  errors.text(errorMsg)
+  errors.show()
+}
+
+function showStatusMessage (msg) {
+  hideErrors()
+  let counter = $('#counter')
+  counter.text(msg)
+  counter.show()
+}
+
+function hideStatusMessage () {
+  let counter = $('#counter')
+  counter.hide()
+  counter.text('')
+}
