ae2f70bed47bfec2c28abf1cf338895cfb9bde7c
Lance T <starrazor31@gmail.com>
2018-06-27 23:42:41 -0700

    Removing config file

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ module.exports = {
     reconnect: true
   },
   identity: {
-    username: 'chinnbot', // bot username
-    password: 'oauth:dfvhm92wcfctkt3ke55lta78ye8060', // oauth token with the 'oauth:' prefix
-    oauth: 'dfvhm92wcfctkt3ke55lta78ye8060' // just the oauth token
+    username: 'weebbot', // bot username
+    password: 'oauth:asdasdasdasdasdwqwewqeqw', // oauth token with the 'oauth:' prefix
+    oauth: 'asdasdasdasdasdasdsadasdasdas' // just the oauth token
   },
   // Array of channels for your bot to join
   // Example channel #xwater
   channels: [
-    '#blackmarmalade'
+    '#weebbot'
   ]
 }
